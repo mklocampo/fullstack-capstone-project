72a1cbdf92d6e6e687d2e40afea3dbb3bd0d0bcc
mklocampo <mkl.ocampo@gmail.com>
2026-08-20 16:29:10 -0400

    frontend detailsPage

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route, useNavigate } from 'react-router-dom';
 import MainPage from './components/MainPage/MainPage';
 import LoginPage from './components/LoginPage/LoginPage';
 import RegisterPage from './components/RegisterPage/RegisterPage';
+import DetailsPage from './components/DetailsPage/DetailsPage';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
@@ -13,11 +14,12 @@ function App() {
     <>
         <Navbar/>
         <Routes>
-          {/* the final code will not pass the products to every page, but each page will call the server API */}
-          <Route path="/" element={<MainPage />} />
-          <Route path="/app" element={<MainPage />} />
-          <Route path="/app/login" element={<LoginPage/>} />
-          <Route path="/app/register" element={<RegisterPage/>} />
+            {/* the final code will not pass the products to every page, but each page will call the server API */}
+            <Route path="/" element={<MainPage />} />
+            <Route path="/app" element={<MainPage />} />
+            <Route path="/app/login" element={<LoginPage/>} />
+            <Route path="/app/register" element={<RegisterPage/>} />
+            <Route path="/app/product/:productId" element={<DetailsPage/>} />
         </Routes>
         </>
   );
