0a98c2ed9d226f918cfd18ce2c62cab5acba327b
mklocampo <mkl.ocampo@gmail.com>
2026-08-21 15:43:39 -0400

    backend register

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -24,13 +24,11 @@ app.use(express.json());
 // Route files
 // Gift API Task 1: import the giftRoutes and store in a constant called giftroutes
 //{{insert code here}}
-
 const giftRoutes = require('./routes/giftRoutes');
 
 
 // Search API Task 1: import the searchRoutes and store in a constant called searchRoutes
 //{{insert code here}}
-
 const searchRoutes = require('./routes/searchRoutes');
 
 
@@ -42,13 +40,15 @@ app.use(pinoHttp({ logger }));
 // Use Routes
 // Gift API Task 2: add the giftRoutes to the server by using the app.use() method.
 //{{insert code here}}
-
 app.use('/api/gifts', giftRoutes);
 
 // Search API Task 2: add the searchRoutes to the server by using the app.use() method.
 //{{insert code here}}
-
 app.use('/api/search', searchRoutes);
+
+
+const authRoutes = require('./routes/authRoutes');
+app.use('/api/auth', authRoutes);
 
 
 // Global Error Handler
