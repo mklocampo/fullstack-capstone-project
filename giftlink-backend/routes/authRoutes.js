0bf80eb0dc0f5cfcc47479b024d146f2bdba85e1
mklocampo <mkl.ocampo@gmail.com>
2026-08-22 15:18:31 -0400

    backend update user

--- giftlink-backend/routes/authRoutes.js
+++ giftlink-backend/routes/authRoutes.js
@@ -3,11 +3,13 @@ const express = require('express');
 //const app = express();
 const bcryptjs = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-//const { body, validationResult } = require('express-validator');
 const connectToDatabase = require('../models/db');
 const router = express.Router();
 const dotenv = require('dotenv');
 const pino = require('pino');  // Import Pino logger
+
+//task 1: Use the body,validationResult from express-validator for input validation
+const { body, validationResult } = require('express-validator');
 
 //Step 1 - Task 3: Create a Pino logger instance
 const logger = pino();  // Create a Pino logger instance
@@ -109,6 +111,60 @@ router.post('/login', async (req, res) => {
 
     } catch (e) {
          return res.status(500).send('Internal server error');
+
+    }
+});
+
+// update API
+router.put('/update', async (req, res) => {
+    // Task 2: Validate the input using `validationResult` and return approiate message if there is an error.
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        logger.error('Validation errors in update request', errors.array());
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+        // Task 3: Check if `email` is present in the header and throw an appropriate error message if not present.
+        const email = req.headers.email;
+        if (!email) {
+            logger.error('Email not found in the request headers');
+            return res.status(400).json({ error: "Email not found in the request headers" });
+        }
+
+        // Task 4: Connect to MongoDB
+        const db = await connectToDatabase();
+        const collection = db.collection("users");
+
+        // Task 5: find user credentials in database
+        const existingUser = await collection.findOne({ email });
+        if (!existingUser) {
+            logger.error('User not found');
+            return res.status(404).json({ error: "User not found" });
+        }
+
+        existingUser.firstName = req.body.name;
+        existingUser.updatedAt = new Date();
+
+        // Task 6: update user credentials in database
+        const updatedUser = await collection.findOneAndUpdate(
+            { email },
+            { $set: existingUser },
+            { returnDocument: 'after' }
+        );
+
+        // Task 7: create JWT authentication using secret key from .env file
+        const payload = {
+            user: {
+                id: updatedUser._id.toString(),
+            },
+        };
+        const authtoken = jwt.sign(payload, JWT_SECRET);
+        logger.info('User updated successfully');
+        
+        res.json({authtoken});
+    } catch (e) {
+        return res.status(500).send('Internal server error');
 
     }
 });
